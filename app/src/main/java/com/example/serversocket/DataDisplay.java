814a78d86b7f746e6d31103c64ee04c64ad6e1f3
SurajRDubey <surajrajeshdubey123@gmail.com>
2020-05-09 21:51:29 +0530

    initial commit

---
+++ app/src/main/java/com/example/serversocket/DataDisplay.java
@@ -0,0 +1,7 @@
+package com.example.serversocket;
+
+public interface DataDisplay {
+
+
+    void Display(String message);
+}
---
+++ app/src/main/java/com/example/serversocket/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.serversocket;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements DataDisplay {
+    TextView serverMessage;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        serverMessage= serverMessage.findViewById(R.id.textView);
+    }
+
+    public void connect(View view)
+    {
+
+        MyServer server=new MyServer();
+        server.setEventListener(this);
+        server.startListening();
+
+    }
+    public void Display(String message)
+    {
+        serverMessage.setText(""+message);
+
+
+    }
+
+
+}
